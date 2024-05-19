2afe080537365ca989937b07d83708ef97b35d1e
segou <segooou@gmail.com>
2024-05-19 20:26:34 -0300

    aaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  build: {
-    outDir: './build'
-  },
+  
   plugins: [react()],
   resolve: {
     alias: [
